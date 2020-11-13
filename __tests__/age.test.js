1124394df1221016f051440fc5eec733558da6ec
Garrett Brown <garrettpaulbrown@gmail.com>
2020-11-13 15:59:25 -0800

    Test, good fail, pass should take user age in Earth years and return in Mercury years.

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -12,6 +12,60 @@ describe('Age', () => {
     expect(age.earthLifeExpectancy).toEqual(200);
   })
 
+  test('should take user age in Earth years and return in Mercury years', () => {
+    const age = new Age(100);
+    age.mercuryAge()
+    expect(age.mercury).toEqual(24)
+  })
 
+  // test('should take user age in Earth years and return in Venus years', () => {
+  //   const age = new Age(100);
+  //   age.venusAge()
+  //   expect(age.venus).toEqual()
+  // })
+
+  // test('should take user age in Earth years and return in Mars years', () => {
+  //   const age = new Age(100);
+  //   age.marsAge()
+  //   expect(age.mars).toEqual()
+  // })
+
+  // test('should take user age in Earth years and return in Jupiter years', () => {
+  //   const age = new Age(100);
+  //   age.jupiterAge()
+  //   expect(age.jupiter).toEqual()
+  // })
+
+  // test('should return remaining time expected to live on Mercury, subtracting life lived from life expectancy', () => {
+    
+  // })
+
+  // test('should return remaining time expected to live on Venus, subtracting life lived from life expectancy', () => {
+    
+  // })
+
+  // test('should return remaining time expected to live on Mars, subtracting life lived from life expectancy', () => {
+    
+  // })
+
+  // test('should return remaining time expected to live on Jupiter, subtracting life lived from life expectancy', () => {
+    
+  // })
+
+  // test('should return number of years lived past Mercury life expectancy', () => {
+    
+  // })
+
+  // test('should return number of years lived past Venus life expectancy', () => {
+    
+  // })
+
+  // test('should return number of years lived past Mars life expectancy', () => {
+    
+  // })
+
+  // test('should return number of years lived past Jupiter life expectancy', () => {
+    
+  // })
 
 });
