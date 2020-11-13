8d0682d1457f9d9b0d97ec76fbc66478bf7490da
Garrett Brown <garrettpaulbrown@gmail.com>
2020-11-13 14:42:54 -0800

    Should return users age in Earth Years. pass.

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -1,17 +1,11 @@
-import { describe } from 'yargs';
 import { Age } from './../src/age.js';
 
 describe('Age', () => {
+
   test('should return users age in Earth years', () => {
     const age = new Age(100);
     expect(age.earth).toEqual(100);
-  })
-
-  test('should return life expectancy in Earth years', () => {
-    const age = new Age(100,200);
-    expect(age.earthLifeExpectancy).toEqual(200);
-  })
-
+  });
 
 
 
