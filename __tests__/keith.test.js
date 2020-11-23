2b6dfb3cdea6d6a0d3a1914ba7b04c0cbc788573
Garrett Brown <garrettpaulbrown@gmail.com>
2020-11-22 16:48:04 -0800

    Fix test.

--- __tests__/keith.test.js
+++ __tests__/keith.test.js
@@ -3,15 +3,15 @@ import { Keith } from './../src/keith.js';
 describe('Keith', () => {
 
   test('should return Keith Richards age in Earth years in the year 2073', () => {
-    const age = Age(130);
+    const age = new Age(130);
     expect(age.earth).toEqual(130);
   });
   test('should return Keith Richards age in the year 2073, in dog years', () => {
-    const age = Age(130);
+    const age = new Age(130);
     expect(age.dogYears).toEqual(910);
   });
   test('should return Keith Richards age in the year 2073, in dog years, in Jupiter years', () => {
-    const age = Age(130);
-    expect().toEqual(10792.6);
+    const age = new Age(130);
+    expect(age.Jupiter).toEqual(10792.6);
   });
 });
