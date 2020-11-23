640416a7cd5e1032e6ae7cacc804e5d3d1d941e8
Garrett Brown <garrettpaulbrown@gmail.com>
2020-11-22 16:45:41 -0800

    Add tests for Keith.

--- __tests__/keith.test.js
+++ __tests__/keith.test.js
@@ -2,23 +2,16 @@ import { Keith } from './../src/keith.js';
 
 describe('Keith', () => {
 
-  test('should return Keith Richards age in Earth years', () => {
-    const age = Age(130)
-    expect(age.earth).toEqual(130)
-  })
-
   test('should return Keith Richards age in Earth years in the year 2073', () => {
-    const age = Age()
-    expect().toEqual()
-  })
-
-  test('should return Keith Richards age in Jupiter years in the year 2073', () => {
-    const age = Age()
-    expect().toEqual()
-  })
-
-  test('should return Keith Richards age in Jupiter years, in dog years, in the year 2073', () => {
-    const age = Age()
-    expect().toEqual()
-  })
+    const age = Age(130);
+    expect(age.earth).toEqual(130);
+  });
+  test('should return Keith Richards age in the year 2073, in dog years', () => {
+    const age = Age(130);
+    expect(age.dogYears).toEqual(910);
+  });
+  test('should return Keith Richards age in the year 2073, in dog years, in Jupiter years', () => {
+    const age = Age(130);
+    expect().toEqual(10792.6);
+  });
 });
