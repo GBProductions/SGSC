59543f853879f32747604cc031a1fc1383b21bfc
Garrett Brown <garrettpaulbrown@gmail.com>
2020-11-22 19:26:02 -0800

    Add template literals to main.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
+import $ from 'jquery';
 import 'bootstrap';
 import './css/styles.css';
+// import { Age } from 'js/age.js';
+// import { Keith } from 'js/keith.js';
 
+$(document).ready(function() {
 
+  let earthAge = 130;
+  let dogYears = earthAge * 7;
+  let jupiterAge = dogYears * 11.86;
+  let sentence = `Keith Richards is ${dogYears} in dog years on Earth. However, in dog years on Jupiter, he is ${jupiterAge}.`;
+
+  console.log(sentence);
+});
