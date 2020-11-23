c8583de1bf8323bd57710e351aa5b22ad3517b2f
Garrett Brown <garrettpaulbrown@gmail.com>
2020-11-22 17:41:39 -0800

    Test failed.

--- __tests__/keith.test.js
+++ __tests__/keith.test.js
@@ -6,10 +6,10 @@ describe('Keith', () => {
     const keith = new Keith(130);
     expect(keith.earth).toEqual(130);
   });
-  // test('should return Keith Richards age in the year 2073, in dog years', () => {
-  //   const age = new Age(130);
-  //   expect(age.dogYears).toEqual(910);
-  // });
+  test('should return Keith Richards age in the year 2073, in dog years', () => {
+    const age = new Age(130);
+    expect(age.dogYears).toEqual();
+  });
   // test('should return Keith Richards age in the year 2073, in dog years, in Jupiter years', () => {
   //   const age = new Age(130);
   //   expect(age.Jupiter).toEqual(10792.6);
